92a6f96e9206cc29636edf412e7a323c61e4f842
Mykyta Kot <mykyta.kot@student.tuke.sk>
2023-11-25 17:09:13 +0100

    login operational

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -9,7 +9,17 @@ const RegistrationHandler = async (req, res) => {
     var email = req.query.email;
     var car_number = req.query.car_number;
     var car_name = req.query.car_name;
+    var password = generateRandomPassword();
+    var data = {
+        name: name,
+        surname: surname,
+        is_admin: false,
+        social_score: 100,
+        email: email,
+        password: password,
+    };
     
+
     if (name && surname && email && car_number && car_name) {
         // All variables exist and are not null
         const emailValid = endsWith(email, 'tsystems.sk');
@@ -22,6 +32,18 @@ const RegistrationHandler = async (req, res) => {
         res.status(400).json({ status: 'error', message : 'On of the fields are empty' });
     }
 };
+const generateRandomPassword = () => {
+    const length = 10;
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters.charAt(randomIndex);
+    }
+    return password;
+};
+
+
 
 const LoginHandler = async (req, res) => {
     var email = req.query.email;
@@ -30,9 +52,23 @@ const LoginHandler = async (req, res) => {
         // All variables exist and are not null
         const emailValid = endsWith(email, 'tsystems.sk');
         if (emailValid) {
-            res.status(200).json({ data: req.query, status: 'success' });
+
+            var user = await getUserByEmail(email);
+            
+            if(user){
+                if(user.password == password){
+                    res.status(200).json({ data: user, status: 'success' });
+                }else{
+                    res.status(400).json({ status: 'error' , message: 'wrong password' });
+                    
+                }
+                
+            }else{
+                res.status(400).json({ status: 'error' , message: 'user not found' });
+            }
+            
         } else {
-            res.status(400).json({ status: 'error' , message: 'wrong email' });
+            res.status(400).json({ status: 'error' , message: 'wrong email host' });
         }
     } else {
         res.status(400).json({ status: 'error' });
